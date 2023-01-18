92c094e71cd82b32aee706ed9989888930fac508
Mikhail Aksenov <fallenangelofdark355@gmail.com>
2023-01-18 16:33:17 +0700

    Add descriptions to variable files

--- packer/variables.pkr.hcl
+++ packer/variables.pkr.hcl
@@ -1,40 +1,48 @@
 variable "source_image_family" {
   type = string
   default = "centos-7"
+  description = "Source image family name to create custom image"
 }
 
 variable "ssh_username" {
   type = string
   default = "centos"
+  description = "SSH User"
 }
 
 variable "use_ipv4_nat" {
   type = string
   default = "true"
+  description = "NAT to get external IP (true or false value)"
 }
 
 variable "image_description" {
   type = string
   default = "Centos 7 Nginx Image"
+  description = "Description of Packer Image"
 }
 
 variable "image_family" {
   type = string
   default = "centos-7-nginx"
+  description = "Packer image family"
 }
 
 variable "image_name" {
   type = string
   default = "nginx"
+  description = "Packer image name"
 }
 variable "image_tag" {
   type = number
   default = 1
+  description = "Packer image tag"
 }
 
 variable "disk_type" {
   type = string
   default = "network-hdd"
+  description = "Disk using template"
 }
 
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,16 @@
 variable "instance_group_name" {
   type        = string
+  description = "Name of the compute instance group"
 }
 
 variable "labels" {
   type 	      = map(string)
+  description = "Labels to add to resources"
 }
 
 variable "cidr_blocks" {
   type	      = list(list(string))
+  description = "List of lists of IPv4 CIDR blocks for subnets"
 }
 
 variable "resources" {
@@ -16,16 +19,19 @@ variable "resources" {
     cpu = number
     memory = number
   })
+    description = "VMs resources: CPU, Memory and Disk size"
 }
 
 variable "public_ssh_key_path" {
   type = string
   default = ""
+  description = "SSH keypair path"
 }
 
 variable "private_ssh_key_path" {
   type = string
   default = ""
+  description = "SSH keypair path"
 }
 
 variable "az" {
@@ -35,48 +41,60 @@ variable "az" {
     "ru-central1-b",
     "ru-central1-c"
   ]
+  description = "Availability Zones"
 }
 
 variable "scale_count" {
   type = number
+  description = "Number of VM to create"
 }
 
 variable "folder_id" {
   type = string
+  description = "Yandex Cloud Folder ID (exported from environment variables)"
 }
 
 variable "image_name" {
   type = string
+  description = "Image name"
 }
 
 variable "image_tag" {
   type = number
+  description = "Image tag"
 }
 
 variable "target_group_name" {
   type = string
+  description = "Application load balancer target group name"
 }
 
 variable "loadbalancer_name" {
   type = string
+  description = "Application load balancer name"
 }
 
 variable "http_router_name" {
   type = string
+  description = "Applictaion load balancer HTTP router name"
 }
 
 variable "backend_group_name" {
   type = string
+  description = "Application load balancer backend group name"
 }
 
 variable "virtual_host_name" {
   type = string 
+  description = "Application load balancer virtual host name"
 }
 
 variable "lb_frontend_port" {
   type = number
+  description = "Application load balancer frontend port"
 }
 
 variable "http_backend_port" {
   type = number
+  description = "Application load balancer backend port"
 }
